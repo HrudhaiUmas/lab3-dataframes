217825c3238828fc94f437aca0af8d4363ab4daa
HrudhaiUmas <hrudhaiumas20@gmail.com>
2023-02-01 10:34:17 -0800

    class just ended, finished half of it

--- lab.R
+++ lab.R
@@ -9,38 +9,55 @@
 ## Use an appropriate variable name for the scores
 ## 
 ## Hint: feel free to invent if you cannot figure this out
+seahawks_first_five_game_scores <- c(17, 7, 23, 48, 32)
 
 
 ## Create a vector of the number of points the opponent
 ## scored against Seahawks in the first 5 games
 ## use an appropriate variable name
 
+opponents_first_five_game_scores <-  c(16, 27, 27, 45, 39)
 
 ## Combine your two vectors into a dataframe
+seahawks_football_scores <- data.frame(seahawks_first_five_game_scores, opponents_first_five_game_scores)
 
 
 ## Create a new column "diff" that is the difference in points
 ## (in favor of Hawks)
+seahawks_football_scores$diff <- (seahawks_football_scores$seahawks_first_five_game_scores - seahawks_football_scores$opponents_first_five_game_scores)
+seahawks_football_scores
+
 
 
 ## Create a new column "won" which is TRUE if the Seahawks won,
 ## ie if Seahawks scored more than the opponent scored against them
+seahawks_football_scores$won <- (seahawks_first_five_game_scores > opponents_first_five_game_scores)
+seahawks_football_scores
 
 
 ## Create a vector of the opponents name (such as "Denver Broncos")
+opponents_name <- c("Denver Broncos", "San Francisco 49ers", "Atlanta Falcons", "Detroit Lions", "New Orleans Saints")
 
 
 ## Add the vector of opponents into the data frame
+seahawks_football_scores$opponents <- opponents_name
+seahawks_football_scores
 
 
 ## Compute the average score of Seahawks
+average_score <- mean(seahawks_football_scores$seahawks_first_five_game_scores)
+average_score
 
 
 ## Compute how many games did Seahawks won
 ## (use the 'won' variable to compute it)
+seahwaks_won_games <- sum(seahawks_football_scores$won == TRUE)
+seahwaks_won_games
 
 
 ## What was the largest difference in scores (in favor of Seahawks)?
+largest_difference <- max(seahawks_football_scores$diff)
+largest_difference
 
 
 ## How many different opponents did Seahawks have in these games?
